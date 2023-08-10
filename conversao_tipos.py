16f83a4b6cb536049bb2a8e4809093a5bdbc4f59
Guilherme <contatoguigvd@hotmail.com>
2023-08-10 12:12:19 -0300

    add conversão

---
+++ conversao_tipos.py
@@ -0,0 +1,14 @@
+# CONVERSÃO DE TIPOS
+
+# int()
+# str()
+# float()
+# bool()
+
+idade = 26
+
+print(idade, type(idade ))
+
+idade_inteira = int(idade)
+
+print(idade_inteira, type(idade_inteira ))
