826b2181dd624caef184925db0cc1e39a612b455
Guilherme <contatoguigvd@hotmail.com>
2023-08-10 20:56:15 -0300

    add funcao

---
+++ funcao.py
@@ -0,0 +1,19 @@
+
+def saudacao():
+    print('Hello World!')
+
+saudacao()
+saudacao()
+
+
+def saudacao2(name):
+    print(f'Bem vindo {name}!!')
+
+saudacao2('Daniel')
+
+
+def somar(n1, n2):
+    return n1+n2
+
+res = somar(1, 8)
+print(res)
