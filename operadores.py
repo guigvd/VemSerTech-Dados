6b82ba8cc88023bd2a47af62dc9ea4f37c398c33
Guilherme <contatoguigvd@hotmail.com>
2023-08-10 11:13:50 -0300

    "add operadores"

---
+++ operadores.py
@@ -0,0 +1,27 @@
+# OPERADORES ARITIMÉTICOS
+
+n1 = 10
+n2 = 20
+
+print(n1 + n2)
+print(n1 - n2)
+print(n1 * n2)
+print(n1 / n2)
+print(n1 // n2) #IGNORA OS DECIMAIS
+print(n1 % n2)
+print(n1 ** n2)
+
+# OPERADORES BOOLEANOS
+
+idade1 = 10
+idade2 = 15
+altura1 = 1.77
+altura2 = 1.65
+altura3 = altura2
+
+print(idade1 > idade2)
+print(idade1 < idade2)
+print('Python' == 'python')
+print('banana' != 'abacaxi')
+print(altura1 >= altura2)
+print(altura2 > altura3)
