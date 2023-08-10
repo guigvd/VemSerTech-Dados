f95415ead78012bce2adb2298d7cbe29674b8e87
Guilherme <contatoguigvd@hotmail.com>
2023-08-10 11:02:51 -0300

    "add var"

---
+++ var.py
@@ -0,0 +1,12 @@
+# VARIÁVEIS
+
+idade = 26
+altura = 1.88
+nome = 'Daniel'
+estudando = True
+
+print(type(idade))
+print(type(estudando))
+
+nome = input("Digite seu nome: ")
+print("Seu nome é: ", nome)
