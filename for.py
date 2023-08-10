6263792a0e6908643acfe9308fd55eeb965e9898
Guilherme <contatoguigvd@hotmail.com>
2023-08-10 15:58:23 -0300

    add for

---
+++ for.py
@@ -0,0 +1,22 @@
+
+# for i in range(10):
+#     print(i)
+
+# print('---------')
+
+# for j in range(4, 8):
+#     print(j)
+
+# print('---------')
+
+# for k in range(1, 10, 3):
+#     print(k)
+
+
+soma = 0
+
+for i in range(1, 4):
+    nota = float(input(f'Digite a nota {i}: '))
+    soma = soma + nota
+
+print(soma / 3)
